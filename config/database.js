6803ab8990f52a9b9e07723f82908d525bd7c704
Ian Josef <ianjosef.rivera@ymail.com>
2020-11-27 20:45:19 +0800

    heroku update

--- config/database.js
+++ config/database.js
@@ -1,15 +1,22 @@
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: "postgres",
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        charset: "utf8",
+        ssl: { rejectUnauthorized: false },
       },
-      options: {
-        useNullAsDefault: true,
-      },
+      options: {},
     },
   },
 });
